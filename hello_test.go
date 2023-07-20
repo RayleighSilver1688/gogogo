14dc4b4ed033f123ecd046bca52159341f111c6a
LAPTOP-KR1DSVN4\Rayleigh <rayleighsilver1688@gmail.com>
2023-07-20 15:14:51 +0800

    Changes

--- hello_test.go
+++ hello_test.go
@@ -4,9 +4,13 @@ import "testing"
 
 func TestHello(t *testing.T) {
 	got := Hello()
-	want := "Hello, world1"
+	want := "Hello, world"
 
 	if got != want {
+		/*
+			%q为  占位符、 点进去Errorf 这个函数、就可以看到，如果结果是错的，就会把got和want这两个参数，放到%q的位置拼接起来
+			作用是：方便查看为什么测试结果是错的。
+		*/
 		t.Errorf("got '%q' want '%q'", got, want)
 	}
 }
